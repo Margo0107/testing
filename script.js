73b072f673982846cd01f69e6dc12642294a9338
Margo0107 <megi.busenka@gmail.com>
2025-11-26 04:35:22 +0200

    traning modal

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
-const input = document.querySelector(".input-text");
+function Modal(modalSelector) {
+  const modal = document.querySelector(modalSelector);
 
-const inputValue = input.value.trim();
-const badRegexp = /(убийство|блять|сука|тварь|пошел нахуй|тупой)/i;
-//bad speack
+  this.init = function (openSelector, closeSelector) {
+    const openButton = document.querySelector(openSelector);
+    openButton.addEventListener("click", this.openModal);
 
-input.addEventListener("input", () => {
-  if (badRegexp.test(inputValue)) {
-    const error = input.nextElementSibling();
-    error.textContent = "такие слова не допустими!";
-    console.log("такие слова не допустими!");
-  } else {
-    error.textContent = "все ок";
-    console00.log("все ок");
-  }
-});
+    const closeButton = document.querySelector(closeSelector);
+    closeButton.addEventListener("click", this.closeModal);
+
+    window.addEventListener("click", () => this.closeModalOutside(event));
+  };
+  this.openModal = function () {
+    modal.classList.add("modal__open");
+  };
+  this.closeModal = function () {
+    modal.classList.remove("modal__open");
+  };
+  this.closeModalOutside = function (event) {
+    if (event.target === modal) {
+      this.closeModal();
+    }
+  };
+}
+const modal = new Modal(".modal");
+modal.init(".modal-btn-open", ".modal-close");
+
+const modal2 = new Modal(".modal");
+modal2.init(".modal-btn-open2", ".modal-close");
