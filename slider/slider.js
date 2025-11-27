7770258e6557b7ee3ce4908e4ed589e6807a4e71
margo <megi.busenka@gmail.com>
2025-11-27 18:20:47 +0200

    add slider galery

---
+++ slider/slider.js
@@ -0,0 +1,40 @@
+const sliderBox = document.querySelector(".slider");
+const firstImg = sliderBox.querySelectorAll("img")[0];
+const arrowIcon = document.querySelectorAll(".wrapper i");
+
+//switching forward and backward
+let firstWitdth = firstImg.clientWidth + 20;
+
+arrowIcon.forEach((icon) => {
+  icon.addEventListener("click", () => {
+    sliderBox.scrollLeft += icon.id == "left" ? -firstWitdth : firstWitdth;
+  });
+});
+
+//drag the slider along the container
+let isDown = false;
+let startX;
+let scrollLeft;
+
+sliderBox.addEventListener("mousedown", (e) => {
+  isDown = true;
+  sliderBox.classList.add("active");
+  startX = e.pageX - sliderBox.offsetLeft;
+  scrollLeft = sliderBox.scrollLeft;
+});
+
+sliderBox.addEventListener("mouseleave", () => {
+  isDown = false;
+});
+
+sliderBox.addEventListener("mouseup", () => {
+  isDown = false;
+});
+
+sliderBox.addEventListener("mousemove", (e) => {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - sliderBox.offsetLeft;
+  const walk = x - startX;
+  sliderBox.scrollLeft = scrollLeft - walk;
+});
