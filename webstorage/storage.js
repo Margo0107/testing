4ef6389e842ab8b3a280febdded8df3de5c2a60c
margo <megi.busenka@gmail.com>
2025-12-01 15:18:31 +0200

    add todo

--- webstorage/storage.js
+++ webstorage/storage.js
@@ -1,4 +1,3 @@
-
 const button = document.querySelector("button");
 
 button.addEventListener("click", () => {
@@ -9,3 +8,32 @@ button.addEventListener("click", () => {
 if (localStorage.getItem("theme") === "dark") {
   document.body.classList.add("dark");
 }
+//todo
+
+const input = document.querySelector(".input-text");
+const btnSave = document.querySelector(".save");
+const result = document.querySelector(".result");
+
+let notes = JSON.parse(localStorage.getItem("notes")) || [];
+
+function render() {
+  result.replaceChildren();
+
+  notes.forEach((item) => {
+    const newLi = document.createElement("li");
+    newLi.textContent = item;
+    result.append(newLi);
+  });
+}
+render();
+
+btnSave.addEventListener("click", () => {
+  const text = input.value.trim();
+  if (!text) return;
+
+  notes.push(text);
+  localStorage.setItem("notes", JSON.stringify(notes));
+
+  input.value = "";
+  render();
+});
