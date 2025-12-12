18513203794d37d69122cc6aafdf0ef49e361193
margo <megi.busenka@gmail.com>
2025-12-12 13:31:06 +0200

    add heart localStorage

--- store/store.js
+++ store/store.js
@@ -1,10 +1,11 @@
-function Product(img, price, title, brand) {
+function Product(id, img, price, title, brand) {
+  this.id = id;
   this.img = img;
   this.price = price;
   this.title = title;
   this.brand = brand;
 }
-
+const likes = JSON.parse(localStorage.getItem("likes")) || {};
 Product.prototype.render = function () {
   const card = document.createElement("div");
   card.classList.add("card");
@@ -21,13 +22,35 @@ Product.prototype.render = function () {
 
   const span = document.createElement("span");
   span.textContent = this.brand;
-  card.append(img, h2, p, span);
+
+  const iconHeart = document.createElement("i");
+
+  if (likes[this.id]) {
+    iconHeart.classList.add("fa-solid", "fa-heart");
+  } else {
+    iconHeart.classList.add("fa-regular", "fa-heart");
+  }
+
+  iconHeart.addEventListener("click", () => {
+    if (iconHeart.classList.contains("fa-regular")) {
+      iconHeart.classList.remove("fa-regular");
+      iconHeart.classList.add("fa-solid");
+      likes[this.id] = true;
+    } else {
+      iconHeart.classList.add("fa-regular");
+      iconHeart.classList.remove("fa-solid");
+      likes[this.title.id] = false;
+    }
+    localStorage.setItem("likes", JSON.stringify(likes));
+  });
+
+  card.append(img, h2, p, span, iconHeart);
   return card;
 };
 
 const root = document.querySelector(".root");
-const p1 = new Product("img/p1.jpg", 235, "Air Zoom Alphafly", "Nike");
-const p2 = new Product("img/p2.jpg", 250, "Air Zoom Alphafly", "abibas");
-const p3 = new Product("img/p3.jpg", 250, "Air Zoom Alphafly", "abibas");
-const p4 = new Product("img/p4.jpg", 280, "Air Zoom Alphafly", "abibas");
+const p1 = new Product(1, "img/p1.jpg", 235, "Air Zoom Alphafly", "Nike");
+const p2 = new Product(2, "img/p2.jpg", 250, "Air Zoom Alphafly", "abibas");
+const p3 = new Product(3, "img/p3.jpg", 250, "Air Zoom Alphafly", "abibas");
+const p4 = new Product(4, "img/p4.jpg", 280, "Air Zoom Alphafly", "abibas");
 root.append(p1.render(), p2.render(), p3.render(), p4.render());
