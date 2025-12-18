cc558f3322bdf0109ca36b6ed11fb9d7db51efdc
Margo0107 <megi.busenka@gmail.com>
2025-12-18 07:47:49 +0200

    add modal

--- stripe/script.js
+++ stripe/script.js
@@ -1,57 +1,30 @@
-class Product {
-  #name;
-  #price;
-  constructor(name, price) {
-    this.#name = name;
-    this.#price = price;
-  }
-  get name() {
-    return this.#name;
-  }
-  get price() {
-    return this.#price;
-  }
-  set name(newName) {
-    if (newName.length >= 3) {
-      this.#name = newName;
-    }
-  }
-  set price(newPrice) {
-    if (newPrice > 0) {
-      this.#price = newPrice;
-    }
+class Modal {
+  constructor(modalSelector, openModal, closeModal) {
+    this.modal = document.querySelector(modalSelector);
+    this.openModal = document.querySelectorAll(openModal);
+    this.closeModal = document.querySelectorAll(closeModal);
+
+    this.addEvent();
   }
 
-  showInfo() {
-    return `${this.name} - ${this.price}`;
+  addEvent() {
+    this.openModal.forEach((btn) => {
+      btn.addEventListener("click", () => this.open());
+    });
+
+    this.closeModal.forEach((btnClose) => {
+      btnClose.addEventListener("click", () => this.close());
+    });
+
+    this.modal.addEventListener("click", (e) => {
+      if (e.target === this.modal) this.close();
+    });
   }
-  render(containerSelector) {
-    const container = document.querySelector(containerSelector);
-
-    const card = document.createElement("div");
-    card.classList.add("card");
-
-    const h2 = document.createElement("h2");
-    h2.textContent = this.name;
-
-    const span = document.createElement("span");
-    span.textContent = this.price;
-
-    const p = document.createElement("p");
-    p.textContent = this.size;
-    card.append(h2, span, p);
-    container.append(card);
+  open() {
+    this.modal.classList.add("active");
+  }
+  close() {
+    this.modal.classList.remove("active");
   }
 }
-
-class Shoes extends Product {
-  constructor(name, price, size) {
-    super(name, price);
-    this.size = size;
-  }
-  showInfo() {
-    return `${super.showInfo()} розмер: ${this.size}`;
-  }
-}
-
-const product1 = new Shoes("iaiaiaia", 456, 38).render(".container");
+new Modal(".modal", ".open-modal", ".close");
