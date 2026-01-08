441d049f2c3fb05706c6dd62efa2b68b6031f994
margo <megi.busenka@gmail.com>
2026-01-08 06:51:09 +0200

    a Christmas tree with snowflakes was made

--- webpack-basics/src/js/index.js
+++ webpack-basics/src/js/index.js
@@ -1,4 +1,5 @@
-import "../scss/style.scss";
+import "../js/utils";
+import "../scss/styles.scss";
 
 const wrapperTree = document.createElement("div");
 wrapperTree.classList.add("wrapper-tree");
@@ -19,16 +20,16 @@ layerContainer.classList.add("layer-tree");
 //layer generator
 function createLayerTree(level) {
   const form = document.createElement("div");
-  form.classList.add(`form-layer-${level}`);
+  form.classList.add(`form-layer`);
 
   const layerLeft = document.createElement("div");
-  layerLeft.classList.add(`layer-left-${level}`);
+  layerLeft.classList.add(`layer-left`);
   //balls
   const boll = document.createElement("div");
-  boll.classList.add("boll", `${level}`);
+  boll.classList.add(`boll`);
 
   const layerRight = document.createElement("div");
-  layerRight.classList.add(`layer-right-${level}`);
+  layerRight.classList.add(`layer-right`);
 
   layerLeft.append(boll);
   form.append(layerLeft, layerRight);
@@ -47,3 +48,34 @@ steam.classList.add("stem");
 wrapperTree.append(treeContainer);
 treeContainer.append(star, tree);
 tree.append(layerContainer, steam);
+
+const colors = [
+  "rgb(252, 23, 23)",
+  "rgb(255, 153, 0)",
+  "rgb(255, 214, 33)",
+  "rgba(95, 243, 9, 1)",
+  "rgb(33, 255, 207)",
+  "rgb(33, 137, 255)",
+  "rgb(81, 33, 255)",
+  "rgb(203, 33, 255)",
+  "rgb(255, 33, 218)",
+];
+const bollsi = document.querySelectorAll(".boll");
+function pointBolls() {
+  bollsi.forEach((boll, index) => {
+    boll.style.boxShadow = `0px 1px 8px 3px ${colors[index % colors.length]}`;
+    boll.style.backgroundColor = colors[index % colors.length];
+  });
+}
+pointBolls();
+
+let start = 0;
+function runBolls() {
+  bollsi.forEach((boll, index) => {
+    const colorIndex = (index + start) % colors.length;
+    boll.style.boxShadow = `0px 1px 8px 3px ${colors[colorIndex]}`;
+    boll.style.backgroundColor = colors[colorIndex];
+  });
+  start++;
+}
+setInterval(runBolls, 500);
--- webpack-basics/src/js/utils.js
+++ webpack-basics/src/js/utils.js
@@ -1,1 +1,33 @@
+const starContsiner = document.createElement("div");
+starContsiner.classList.add("strs-container");
+document.body.append(starContsiner);
+const numbersStar = 100;
 
+function createStars() {
+  for (let i = 0; i <= numbersStar; i++) {
+    const stars = document.createElement("div");
+    stars.classList.add("stars");
+
+    const size = Math.random() * 1 + 5;
+    stars.style.width = `${size}px`;
+    stars.style.height = `${size}px`;
+    stars.style.opacity = Math.random();
+
+    let y = Math.random() * -window.innerHeight;
+    let x = Math.random() * window.innerWidth;
+    stars.style.left = `${x}px`;
+
+    const fallingSpeed = Math.random() * 0.3 + 0.7;
+
+    const animate = () => {
+      y += fallingSpeed;
+      stars.style.transform = `translateY(${y}px)`;
+      if (y > window.innerHeight) y = -10;
+      requestAnimationFrame(animate);
+    };
+    animate()
+
+    starContsiner.append(stars);
+  }
+}
+createStars();
